04cfcbf6ba72f3df06afca455713a2609444e431
Nick <nicolachiarappa2001@gmail.com>
2024-11-21 17:09:53 +0100

    Initial Commit

---
+++ SmartBulb/SmartBulbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBulbApp.swift
+//  SmartBulb
+//
+//  Created by Nicola Chiarappa on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartBulbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
